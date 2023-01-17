c2cf1ef71306044d9b8c4fa4e34819924f378770
Talha Fayyaz <42764771+talhafayyaz123@users.noreply.github.com>
2023-01-17 12:52:12 +0500

    deployment code

--- resources/js/plugins/axios.js
+++ resources/js/plugins/axios.js
@@ -3,15 +3,16 @@ import axios from 'axios'
 const axiosIns = axios.create({
 // You can add your headers here
 // ================================
- baseURL: 'http://127.0.0.1:8000/',
-// timeout: 1000,
+ //baseURL: 'http://127.0.0.1:8000/',
+ baseURL: 'https://future-estate.talhafayyaz.com/',
+ // timeout: 1000,
 // headers: {'X-Custom-Header': 'foobar'}
 })
 axiosIns.interceptors.request.use( function (config) {
 
     const token = localStorage.getItem('accessToken');
     const tokenType = "Bearer"
-    
+
 
     if (token) {
       config.headers['Authorization'] = `${tokenType} ${token}`;
