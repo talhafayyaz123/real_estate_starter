53ebdf04ff4e69b16adb877c7decffc7f5de4507
Talha Fayyaz <42764771+talhafayyaz123@users.noreply.github.com>
2023-01-17 15:41:36 +0500

    redirect fix

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -9,7 +9,7 @@ const router = createRouter({
   routes: [
 
     {
-      path: '/dashboard',
+      path: '/',
       redirect: to => {
         const userData = JSON.parse(localStorage.getItem('userData') || '{}')
         const userRole = userData && userData.role ? userData.role : null
@@ -23,7 +23,7 @@ const router = createRouter({
       },
     },
     {
-      path: '/',
+      path: '/home',
       name: 'home',
       component: () => import("@/views/pages/landing/home/home-3.vue"),
     },
